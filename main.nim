7ae646e08e27bd010e429c918ee8f13e5fdcc851
Daniel Hertz <daniel@dhertz.com>
2015-03-26 00:39:40 -0400

    got something working maybe with logo. needs cleaning up

--- main.nim
+++ main.nim
@@ -9,24 +9,80 @@ import strutils
 import tables
 import times
 
+proc loadAkaPiLogo(): PSurface
+
 const
     RED = rgb(250,45,39)
     PURPLE = rgb(153,17,153)
     FORECAST_IO = "https://api.forecast.io/forecast/" & FORECAST_IO_KEY & "/42.364452,-71.089179?units=si"
     MBTA_RED_LINE = "http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=" & MBTA_KEY & "&stop=place-knncl&format=json"
 
-proc writePPM(surface: PSurface, f: File) =
-    f.writeln "P6\n", surface.w, " ", surface.h, "\n255"
-    for y in 0..surface.h-1:
-        for x in 0..surface.w-1:
-           var (r, g, b) = surface[(x, y)].extractRGB
-           f.write char(r)
-           f.write char(g)
-           f.write char(b)
+let AKAPI_LOGO:PSurface = loadAkaPiLogo()
 
 proc isPurpleDayz():bool =
     var now = getLocalTime(getTime())
     return (now.weekday == dWed and 3 < now.monthday and now.monthday < 11) or (now.weekday == dFri and (now.monthday < 6 or now.monthday > 12))
+
+template withFile(f: expr, filename: string, mode: FileMode, body: stmt): stmt {.immediate.} =
+    let fn = filename
+    var f: File
+    if open(f, fn, mode):
+        defer: close(f)
+        body
+    else:
+        quit("cannot open: " & fn)
+
+proc loadAkaPiLogo(): PSurface =
+    withFile(AkaPiLogo, "/home/dhertz/AkaPi_logo.ppm", fmRead):
+        if AkaPiLogo.readLine != "P6":
+            raise newException(E_base, "Invalid file format")
+
+        var line = ""
+        while AkaPiLogo.readLine(line):
+            if line[0] != '#':
+              break
+
+        if AkaPiLogo.readLine != "255":
+            raise newException(E_base, "Invalid file format")
+
+        var
+            parts = line.split(" ")
+            (x, y) = (parseInt parts[0], parseInt parts[1])
+            result = newSurface(x, y)
+            arr: array[256, int8]
+            read = AkaPiLogo.readBytes(arr, 0, 256)
+            pos = 0
+
+        while read != 0:
+            var pixelPos = pos div 3
+            result[pixelPos mod x, pixelPos mod y]=rgb(arr[pos].uint8, arr[pos+1].uint8, arr[pos+2].uint8)
+            pos += 3
+
+            read = AkaPiLogo.readBytes(arr, 0, 256)
+        return result
+
+proc makePpmFromString(displayString, filename) =
+    let
+        color = if isPurpleDayz(): PURPLE else: RED
+        font = newFont(name = "/home/dhertz/Downloads/MBTA.ttf", size = 16, color = color)
+
+    var
+        (textWidth, textHeight) = textBounds(displayString, font)
+        surface = newSurface(AKAPI_LOGO.w + textWidth + 15, 18)
+
+    surface.blit((0, 0, AKAPI_LOGO.w, AKAPI_LOGO.h), AKAPI_LOGO, (0, 0, AKAPI_LOGO.w + 5 + textWidth, AKAPI_LOGO.h))
+    surface.drawText((5,1), displayString, font)
+    withfile(f, filename, fmWrite):
+        surface.writePPM(f)
+
+proc writePPM(surface: PSurface, f: File) =
+    f.writeln "P6\n", surface.w, " ", surface.h, "\n255"
+    for y in 0..surface.h-1:
+        for x in 0..surface.w-1:
+            var (r, g, b) = surface[(x, y)].extractRGB
+            f.write char(r)
+            f.write char(g)
+            f.write char(b)
 
 template recurringJob(content, displayString, filename, waitTime: int, url, actions: stmt) {.immediate.} =
     var displayString = ""
@@ -34,32 +90,16 @@ template recurringJob(content, displayString, filename, waitTime: int, url, acti
         proc asyncJob():Future[int] {.async.} =
             while true:
                 let content = try: getContent(url)
-                           except: "Failed to retrieve URL"
-                try: actions
-                except: echo("Failed to create image")
-               
-                let color = if isPurpleDayz(): PURPLE else: RED
-                let font = newFont(name = "/home/dhertz/Downloads/MBTA.ttf", size = 16, color = color)
-                var (textWidth, textHeight) = textBounds(displayString, font)
-                let surface = newSurface(textWidth + 10, 18)
-                surface.drawText((5,1), displayString, font)
-                withfile(f, filename, fmWrite):
-                    surface.writePPM(f)
- 
+                           except: "Failed to retrieve URL:\n\t" & getCurrentExceptionMsg()
+                try:
+                     actions
+                     makePpmFromString(displayString, filename)
+                except:
+                     echo("Failed to create image:\n\t", getCurrentExceptionMsg())
+
                 await sleepAsync(waitTime*1000)
             return 1
         discard asyncJob()
-
-template withFile(f: expr, filename: string, mode: FileMode, body: stmt): stmt {.immediate.} =
-  let fn = filename
-  var f: File
-  if open(f, fn, mode):
-    try:
-      body
-    finally:
-      close(f)
-  else:
-    quit("cannot open: " & fn)
 
 recurringJob(rawWeather, weatherString, "sign_weather.ppm", 600, FORECAST_IO):
     let weather = parseJson(rawWeather)
@@ -75,7 +115,6 @@ recurringJob(rawRealtime, first_in_direction, "sign_T.ppm", 60, MBTA_RED_LINE):
             realtimeSubway = mode
             break
 
-    first_in_direction = ""
     var seen_headsigns = initOrderedTable[string, seq[int]]()
     for route in realtimeSubway["route"]:
         for direction in route["direction"]:
@@ -86,17 +125,18 @@ recurringJob(rawRealtime, first_in_direction, "sign_T.ppm", 60, MBTA_RED_LINE):
                 elif not seen_headsigns.hasKey(trip["trip_headsign"].str):
                      seen_headsigns[trip["trip_headsign"].str] = @[secAway]
                 else:
-                     seen_headsigns[trip["trip_headsign"].str] = seen_headsigns[trip["trip_headsign"].str] & secAway 
+                     seen_headsigns[trip["trip_headsign"].str] = seen_headsigns[trip["trip_headsign"].str] & secAway
 
     for headsign in seen_headsigns.keys:
-        var headsignMinutes: seq[string] = @[]
-        var sortedTimes = seen_headsigns[headsign][0..2]
+        var
+            headsignMinutes: seq[string] = @[]
+            sortedTimes = seen_headsigns[headsign]
+
         sortedTimes.sort(system.cmp[int])
         for x in sortedTimes:
             headsignMinutes.add($round(x/60))
 
         first_in_direction &= headsign & " " & join(headsignMinutes, "m, ") & "m $ "
-
     echo(first_in_direction)
 
-#runForever()
+runForever()
