f9d635b4dcf48de26b8fb57682d0d1400ce3d25d
Daniel Hertz <daniel@dhertz.com>
2015-03-25 21:53:44 -0400

    Made the T ppm slightly work

--- main.nim
+++ main.nim
@@ -1,3 +1,4 @@
+import algorithm
 import asyncdispatch
 import colors
 import graphics
@@ -5,12 +6,14 @@ import httpclient
 import json
 import math
 import strutils
+import tables
 import times
 
 const
-    RED: Color = rgb(250,45,39)
-    PURPLE: Color = rgb(153,17,153)
-    FORECAST_IO: string = "https://api.forecast.io/forecast/" & FORECAST_IO_KEY & "/42.364452,-71.089179?units=si"
+    RED = rgb(250,45,39)
+    PURPLE = rgb(153,17,153)
+    FORECAST_IO = "https://api.forecast.io/forecast/" & FORECAST_IO_KEY & "/42.364452,-71.089179?units=si"
+    MBTA_RED_LINE = "http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=" & MBTA_KEY & "&stop=place-knncl&format=json"
 
 proc writePPM(surface: PSurface, f: File) =
     f.writeln "P6\n", surface.w, " ", surface.h, "\n255"
@@ -25,13 +28,24 @@ proc isPurpleDayz():bool =
     var now = getLocalTime(getTime())
     return (now.weekday == dWed and 3 < now.monthday and now.monthday < 11) or (now.weekday == dFri and (now.monthday < 6 or now.monthday > 12))
 
-template recurringJob(content, waitTime: int, url, actions: stmt) {.immediate.} =
+template recurringJob(content, displayString, filename, waitTime: int, url, actions: stmt) {.immediate.} =
+    var displayString = ""
     block:
         proc asyncJob():Future[int] {.async.} =
             while true:
                 let content = try: getContent(url)
                            except: "Failed to retrieve URL"
-                actions
+                try: actions
+                except: echo("Failed to create image")
+               
+                let color = if isPurpleDayz(): PURPLE else: RED
+                let font = newFont(name = "/home/dhertz/Downloads/MBTA.ttf", size = 16, color = color)
+                var (textWidth, textHeight) = textBounds(displayString, font)
+                let surface = newSurface(textWidth + 10, 18)
+                surface.drawText((5,1), displayString, font)
+                withfile(f, filename, fmWrite):
+                    surface.writePPM(f)
+ 
                 await sleepAsync(waitTime*1000)
             return 1
         discard asyncJob()
@@ -47,17 +61,42 @@ template withFile(f: expr, filename: string, mode: FileMode, body: stmt): stmt {
   else:
     quit("cannot open: " & fn)
 
-recurringJob(rawWeather, 10, FORECAST_IO):
+recurringJob(rawWeather, weatherString, "sign_weather.ppm", 600, FORECAST_IO):
     let weather = parseJson(rawWeather)
-    var weatherString = weather["hourly"]["summary"].str & " Feels like " & $round(weather["currently"]["apparentTemperature"].fnum) & "C"
+    weatherString = weather["hourly"]["summary"].str & " Feels like " & $round(weather["currently"]["apparentTemperature"].fnum) & "C"
     weatherString = weatherString.replace("–", by="-")
     echo(weatherString)
-    let color = if isPurpleDayz(): PURPLE else: RED
-    let font = newFont(name = "/home/dhertz/Downloads/MBTA.ttf", size = 16, color = color)
-    var (textWidth, textHeight) = textBounds(weatherString, font)
-    let surface = newSurface(textWidth + 10, 18)
-    surface.drawText((5,1), weatherString, font)
-    withfile(f, "/home/dhertz/sign_weather.ppm", fmWrite):
-        surface.writePPM(f)
+
+recurringJob(rawRealtime, first_in_direction, "sign_T.ppm", 60, MBTA_RED_LINE):
+    let realtime = parseJson(rawRealtime)
+    var realtimeSubway: JsonNode
+    for mode in realtime["mode"]:
+        if mode["mode_name"].str == "Subway":
+            realtimeSubway = mode
+            break
+
+    first_in_direction = ""
+    var seen_headsigns = initOrderedTable[string, seq[int]]()
+    for route in realtimeSubway["route"]:
+        for direction in route["direction"]:
+            for trip in direction["trip"]:
+                var secAway = parseInt(trip["pre_away"].str)
+                if 200 >= secAway:
+                     continue
+                elif not seen_headsigns.hasKey(trip["trip_headsign"].str):
+                     seen_headsigns[trip["trip_headsign"].str] = @[secAway]
+                else:
+                     seen_headsigns[trip["trip_headsign"].str] = seen_headsigns[trip["trip_headsign"].str] & secAway 
+
+    for headsign in seen_headsigns.keys:
+        var headsignMinutes: seq[string] = @[]
+        var sortedTimes = seen_headsigns[headsign][0..2]
+        sortedTimes.sort(system.cmp[int])
+        for x in sortedTimes:
+            headsignMinutes.add($round(x/60))
+
+        first_in_direction &= headsign & " " & join(headsignMinutes, "m, ") & "m $ "
+
+    echo(first_in_direction)
 
 #runForever()
